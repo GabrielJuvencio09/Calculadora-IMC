c6350d37997bdb3304e39171a0fe1659e9eda895
GabrielJuvencio09 <130175105+GabrielJuvencio09@users.noreply.github.com>
2023-06-07 08:56:11 -0300

    Ajuste no js

--- Javascript/calc.js
+++ Javascript/calc.js
@@ -1,31 +1,35 @@
-const nome = document.querySelector('.nome')
-const altura = document.querySelector('.altura')
-const peso = document.querySelector('.peso')
-const cal = document.querySelector('#calc')
+const nome = document.querySelector('.nome');
+const altura = document.querySelector('.altura');
+const peso = document.querySelector('.peso');
+const cal = document.querySelector('#calc');
+const form = document.querySelector('form');
+
+nome.focus();
 
 form.addEventListener('submit', function(event){
-    event.preventDefault()
-    let input = nome.value
-    let alt = altura.value
-    let pes = peso.value
-    let imc = (pes / [alt * alt])
-    let result = imc.toFixed(3)
- 
-    document.querySelector('.resultado').innerHTML = input + ", seu IMC é <br> " + result
+    event.preventDefault();
+    
+    let input = nome.value;
+    let alt = altura.value;
+    let pes = peso.value;
+    let imc = pes / (alt * alt);
+    let result = imc.toFixed(3);
+    
+    document.querySelector('.resultado').innerHTML = input + ", seu IMC é <br> " + result;
 
-    if(imc <= 24.9 ){
-        document.querySelector('.dica').innerHTML = "Normal"
+    if (imc <= 24.9) {
+        document.querySelector('.dica').innerHTML = "Normal";
     }
 
-    if(imc >= 25.9 ){
-        document.querySelector('.dica').innerHTML = "Sobrepeso"
+    if (imc >= 25.9) {
+        document.querySelector('.dica').innerHTML = "Sobrepeso";
     }
     
-    if(imc >= 30.0 ){
-        document.querySelector('.dica').innerHTML = "Obesidade"
+    if (imc >= 30.0) {
+        document.querySelector('.dica').innerHTML = "Obesidade";
     }
-    if(imc >= 40.0 ){
-        document.querySelector('.dica').innerHTML = "Obesidade Grave"
+    
+    if (imc >= 40.0) {
+        document.querySelector('.dica').innerHTML = "Obesidade Grave";
     }
-})
-   
+});
